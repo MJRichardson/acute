c399a3fed1958f6caaf05c9c2ed202883ce162c4
MJ Richardson <michael.james.richardson@gmail.com>
2013-12-11 17:13:24 +1000

    Added angular RouteProvider class

--- src/Acute/Angular/RouteProvider.cs
+++ src/Acute/Angular/RouteProvider.cs
@@ -1,7 +1,14 @@
-﻿namespace Acute.Angular
+﻿using System;
+
+namespace Acute.Angular
 {
+    [ScriptExternal]
     internal class RouteProvider
     {
-         
+         public RouteProvider when(string path, object route)
+         {
+             return null;
+         }
+
     }
 }
