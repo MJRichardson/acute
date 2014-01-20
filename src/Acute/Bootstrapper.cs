06e2abb7f85e3edbd6393394e852735246f114bc
MJ Richardson <michael.james.richardson@gmail.com>
2014-01-21 08:30:21 +1000

    Added bootstrapper

---
+++ src/Acute/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+namespace Acute
+{
+    public class Bootstrapper
+    {
+        public static void Init()
+        {
+           foreach (var type in Assembly.GetExecutingAssembly().GetTypes())
+           {
+               if (type.IsSubclassOf(typeof (App)) == false)
+                   continue;
+
+               Activator.CreateInstance(type);
+
+           }
+        }
+    }
+}
