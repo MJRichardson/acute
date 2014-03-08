01ea7a3b3a6813428d9a8cab60e4faa661043b0d
mj.richardson <michael.james.richardson@gmail.com>
2014-03-08 14:56:28 +1000

    Added ACuteCompiler MSBuild task and targets file

--- src/Acute.Build/AcuteCompileTask.cs
+++ src/Acute.Build/AcuteCompileTask.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.Build.Utilities;
+﻿using System.Linq;
+using Acute.Compiler;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
 
 namespace Acute.Build
 {
     public class AcuteCompileTask :Task
     {
+        public ITaskItem[] Sources { get; set; }
+        public ITaskItem[] References { get; set; }
+		public string OutputAssembly { get; set; }
+		public string OutputScript { get; set; }
+
         public override bool Execute()
         {
-            throw new System.NotImplementedException();
+            return Acute.Compiler.Driver.Compile(
+                new CompileOptions(Sources.Select(x => x.ItemSpec), References.Select(x => x.ItemSpec), OutputAssembly, OutputScript), 
+                new TaskErrorReporter(Log)
+                );
         }
     }
 }
---
+++ src/Acute.Build/TaskErrorReporter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ICSharpCode.NRefactory.TypeSystem;
+using Microsoft.Build.Utilities;
+using Saltarelle.Compiler;
+
+namespace Acute.Build
+{
+	public class TaskErrorReporter : IErrorReporter {
+		private readonly TaskLoggingHelper _log;
+
+		public TaskErrorReporter(TaskLoggingHelper log) {
+			_log = log;
+		}
+
+		public DomRegion Region { get; set; }
+
+		public void Message(MessageSeverity severity, int code, string message, params object[] args) {
+			if (severity == MessageSeverity.Error) {
+				_log.LogError(null, string.Format("CS{0:0000}", code), null, Region.FileName, Region.BeginLine, Region.BeginColumn, Region.EndLine, Region.EndColumn, message, args);
+			}
+			else {
+				_log.LogWarning(null, string.Format("CS{0:0000}", code), null, Region.FileName, Region.BeginLine, Region.BeginColumn, Region.EndLine, Region.EndColumn, message, args);
+			}
+		}
+
+		public void InternalError(string text) {
+			this.Message(Messages.InternalError, text);
+		}
+
+		public void InternalError(Exception ex, string additionalText = null) {
+			this.Message(Messages.InternalError, (additionalText != null ? additionalText + ": " : "") + ex.ToString());
+		}
+	}
+}
