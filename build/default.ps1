9edac43b0f01a130d5bc68101045d76958f17fe3
MJ Richardson <michael.james.richardson@gmail.com>
2014-03-19 17:31:13 +1000

    Creation of nuget packages for publishing

--- build/default.ps1
+++ build/default.ps1
@@ -6,7 +6,7 @@ properties {
 	$configuration = "Release"
 }
 
-Task default -Depends Build-Compiler
+Task default -Depends Build-Compiler, Build-Library
 
 Task Clean {
 	if (Test-Path $outDir) {
@@ -27,4 +27,31 @@ Task Build-Compiler -Depends Clean {
 	Exec { msbuild "$baseDir\src\compiler.sln" /verbosity:minimal /p:"Configuration=$configuration" }
 	copy $msBuildProjectTargetDir*.* "$outDir"  
 	Exec { & "$baseDir\submodules\saltarelle.compiler\build\EmbedAssemblies.exe" /o "$outDir\$msBuildProjectTargetFileName" /a "$msBuildProjectTargetDir*.dll" /a "$msBuildProjectTargetDir*.exe"  $msBuildProjectTargetDir$msBuildProjectTargetFileName}
+}
+
+Task Nuget-Pack -Depends Nuget-Pack-Library, Nuget-Pack-Project {
+} 
+
+Task Nuget-Pack-Library -Depend Build-Library {
+	copy "$outDir\Acute.dll" "$baseDir\build\nuget\Acute.Library\lib" 
+	copy "$outDir\Acute.js" "$baseDir\build\nuget\Acute.Library\content" 
+	Exec{ & "$baseDir\build\nuget\nuget.exe" pack "$baseDir\build\nuget\Acute.Library\Acute.Library.nuspec"  }
+}
+
+Task Nuget-Pack-Project -Depend Build-Compiler {
+	copy "$outDir\Acute.Build.dll" "$baseDir\build\nuget\Acute\tools" 
+	copy "$outDir\Acute.Compile.targets" "$baseDir\build\nuget\Acute\tools" 
+	copy "$outDir\Acute.Compiler.exe" "$baseDir\build\nuget\Acute\tools" 
+	copy "$outDir\Antlr3.Runtime.dll" "$baseDir\build\nuget\Acute\tools" 
+	copy "$outDir\Castle.Core.dll" "$baseDir\build\nuget\Acute\tools" 
+	copy "$outDir\Castle.Windsor.dll" "$baseDir\build\nuget\Acute\tools" 
+	copy "$outDir\ICSharpCode.NRefactory.CSharp.dll" "$baseDir\build\nuget\Acute\tools" 
+	copy "$outDir\ICSharpCode.NRefactory.dll" "$baseDir\build\nuget\Acute\tools" 
+	copy "$outDir\ICSharpCode.NRefactory.IKVM.dll" "$baseDir\build\nuget\Acute\tools" 
+	copy "$outDir\IKVM.Reflection.dll" "$baseDir\build\nuget\Acute\tools" 
+	copy "$outDir\JavaScriptParser.dll" "$baseDir\build\nuget\Acute\tools" 
+	copy "$outDir\JavaScriptParser.dll" "$baseDir\build\nuget\Acute\tools" 
+	copy "$outDir\Saltarelle.Compiler.dll" "$baseDir\build\nuget\Acute\tools" 
+	copy "$outDir\Saltarelle.Compiler.JSModel.dll" "$baseDir\build\nuget\Acute\tools" 
+	Exec{ & "$baseDir\build\nuget\nuget.exe" pack "$baseDir\build\nuget\Acute\Acute.nuspec" -NoPackageAnalysis}
 }
