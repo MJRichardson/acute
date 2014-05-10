e420a80e421d6a13f17b4a47caa40655abbffe09
mj.richardson <michael.james.richardson@gmail.com>
2014-05-10 12:16:04 +1000

    Added GA page-view tracking

--- acute-doc.js
+++ acute-doc.js
@@ -20,3 +20,13 @@ function scrollSpy(menuSelector, offset){
 
 			$(menuSelector).affix();
 }
+
+function trackPageView() {
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-6250148-2', 'mjrichardson.github.io');
+  ga('send', 'pageview');
+}
