2ccc2621df0326e74865e38d45cf79635279f91a
MJ Richardson <michael.james.richardson@gmail.com>
2014-06-19 14:03:36 +1000

    Directive jasmine test

--- src/Test.Jasmine/spec/DirectiveSpecs.js
+++ src/Test.Jasmine/spec/DirectiveSpecs.js
@@ -1,4 +1,25 @@
 ﻿describe("When creating a directive", function () {
 
+    describe("With a template", function () {
 
+        var element;
+
+        beforeEach(function() {
+            module('Test.Scenarios.TestApp');
+
+            var html = "<div testScenariosDirectivesDirective></div>";
+
+            inject(function($compile, $rootScope) {
+                var scope = $rootScope.$new();
+                element = angular.element(html);
+                var compiled = $compile(element);
+                compiled(scope);
+                scope.$digest();
+            });
+        });
+
+        it("Should set the element content to the template", function() {
+            expect(element.text()).toBe("three blind mice");
+        });
+    });
 });
