4175b6bda74bc8ad69768da48bd9abd43775dbdb
MJ Richardson <michael.james.richardson@gmail.com>
2014-06-26 17:19:46 +1000

    Created DirectiveDomType enum

---
+++ src/Acute/DirectiveDomTypes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Acute
+{
+    [Flags]
+    public enum DirectiveDomTypes
+    {
+        Attribute = 1,
+        Element = 2,
+        Class = 4
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class DirectiveDomTypesAttribute : Attribute
+    {
+        public DirectiveDomTypes DomTypes { get; private set; }
+
+        public DirectiveDomTypesAttribute(DirectiveDomTypes domTypes)
+        {
+            DomTypes = domTypes;
+        }
+    }
+}
