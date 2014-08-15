4cc9045ad80e41c5648a91a4d24c48062ae72ecb
MJ Richardson <michael.james.richardson@gmail.com>
2014-08-15 10:52:39 +1000

    Initial commit of RouteArgs service

---
+++ src/Acute/Services/RouteArgs.cs
@@ -0,0 +1,29 @@
+﻿using Acute.Angular;
+
+namespace Acute.Services
+{
+    public interface IRouteArgs
+    {
+        dynamic Bag { get; }
+
+        T As<T>() where T : class;
+    }
+
+    public class RouteArgs : IRouteArgs
+    {
+        private readonly RouteParams _routeParams;
+
+        internal RouteArgs(Angular.RouteParams routeParams)
+        {
+            _routeParams = routeParams;
+            Bag = _routeParams;
+        }
+
+        public dynamic Bag { get; private set; }
+
+        public T As<T>() where T : class
+        {
+            return  _routeParams as T;
+        }
+    }
+}
